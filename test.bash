edff686dd8b8ddff426c4ccbad72efc09e98c6a9
kyo yamashita <s21c1135sc@s.chibakoudai.jp>
2022-10-27 12:26:25 +0900

    test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Kyo Yamashita
+# SPDX-License-Identifier: GPL-3.0
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+[ "$res" = 0 ] && echo OK
+exit $res
